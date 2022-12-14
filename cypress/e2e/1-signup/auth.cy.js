12d879c4f41a411119f9e65ad5b5c40a7c50e7a1
duran-thomas <duran.d.thomas@outlook.com>
2022-12-13 22:27:05 -0500

    feat: login automation

---
+++ cypress/e2e/1-signup/auth.cy.js
@@ -0,0 +1,24 @@
+import loginData from './../../fixtures/signin.json'
+import loginPage from './../../page/loginPage'
+
+describe('Should attempt to login with', () => {
+    beforeEach(() => {
+        cy.visit('https://ui-automation-camp.vercel.app')
+    })
+
+    loginData.forEach(element => {
+        it(`${element.userType}`, () => {
+            loginPage.elements.signInBtn().click()
+            loginPage.login(element.email, element.password)
+            if(element.userType === 'valid'){
+                cy.url().should('eq', 'https://ui-automation-camp.vercel.app/products')
+            }
+            if(element.userType === 'invalid password'){
+                loginPage.elements.topErrorMessage().should('have.text', element.message)
+            }
+            if(element.userType === 'invalid username'){
+                loginPage.elements.emailErrorMessage().should('have.text', element.message)
+            }
+        })
+    })
+})
