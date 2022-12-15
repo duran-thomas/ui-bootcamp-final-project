2ddc58d52e86533efff4fec17c5f7eda96681bc3
duran-thomas <duran.d.thomas@outlook.com>
2022-12-15 08:09:13 -0500

    feat: add mocha-awesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    baseUrl: 'https://ui-automation-camp.vercel.app',
+    baseUrl: "https://ui-automation-camp.vercel.app",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
